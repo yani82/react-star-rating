7bd660464acb637b9d794c84cb16d29f389c44ea
yani82 <yani.iman1@gmail.com>
2022-02-18 00:52:58 -0500

    need to replace images

--- react-star-rating/src/App.js
+++ react-star-rating/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import EmptyStar from './empty-star.svg'
+import FilledStar from './filled-star.svg'
 import './App.css';
 
-function App() {
+const Star = ({hoverState, setHoverState, id, rating, setRating}) => {
+  const handleClick = () => {
+    if (id === rating-1) {
+      setRating(0)
+    } else {
+      setRating(id+1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <img onScroll={(hoverState > id || rating > id) ? FilledStar : EmptyStar} alt="empty star"
+    onMouseEnter={() => setHoverState(id+1)}
+    onMouseLeave={() => setHoverState(0)}
+    onClick={handleClick}
+    />
+  )
 }
 
-export default App;
+  const RatingSystem = () => {
+    // destructing the return values from the useState()
+    const [rating, setRating] = useState(0);
+    const [hoverState, setHoverState] = useState(0);
+
+    const stars = [1, 2, 3, 4, 5]
+
+    return (
+      <div>
+        <h1>5 Star Rating System</h1>
+        <h2>Select a rating:</h2>
+          {stars.map((star, i) => (
+          <Star 
+            key={i}
+            // did not recognize the key, so had to set that to 'id'
+            id={i}
+            hoverState = {hoverState}
+            setHoverState = {setHoverState}
+            rating = {rating}
+            setRating = {setRating}
+            />
+        ))}
+      </div>
+    );
+  }
+
+export default function App() {
+  return (
+    <div className='App'>
+      <RatingSystem />
+    </div>
+  )
+};
